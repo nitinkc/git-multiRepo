206cf9accce43f59bba71842f1ca440516c54c0d
Nitin Chaurasia <gs.nitin@gmail.com>
2023-11-11 19:38:47 -0700

    module added for experimentation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# git-tests
+# Multi repo project
+
+Bitbucket Repo : https://nitinc@bitbucket.org/nitinc/git-tests-bb.git 
+
+Github Repo : https://github.com/nitinkc/git-tests.git
+
---
+++ java-module/src/main/java/org/learn/Main.java
@@ -0,0 +1,12 @@
+package org.learn;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "Creating a Fake String";
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
